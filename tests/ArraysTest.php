d87648c19cb26f3ed2364afdb31de64df486baf5
Awilum <awilum@msn.com>
2020-11-24 12:54:03 +0300

    improve tests for sortBySubKey() method.

--- tests/ArraysTest.php
+++ tests/ArraysTest.php
@@ -299,6 +299,56 @@ test('test sortBySubKey() method', function (): void {
     };
 
     $this->assertFalse($array_equal($movies, $arrays_result));
+
+    $arrays_original = Arrays::create([
+        'movies' => [
+            'the_thin_red_line' => [
+                'title' => 'The Thin Red Line',
+                'directed_by' => 'Terrence Malick',
+                'produced_by' => 'Robert Michael, Geisler Grant Hill, John Roberdeau',
+                'decription' => 'Adaptation of James Jones autobiographical 1962 novel, focusing on the conflict at Guadalcanal during the second World War.',
+            ],
+            'bad_times_at_the_el_royale' => [
+                'title' => 'Bad Times at the El Royale',
+                'directed_by' => 'Drew Goddard',
+                'produced_by' => 'Drew Goddard, Steve Asbell',
+                'decription' => 'Early 1970s. Four strangers check in at the El Royale Hotel. The hotel is deserted, staffed by a single desk clerk. Some of the new guests reasons for being there are less than innocent and some are not who they appear to be.',
+            ],
+        ],
+    ]);
+    $movies          = $arrays_original->get('movies');
+    $arrays_result   = Arrays::create($movies)->sortBySubKey('title', 'ASC', SORT_NATURAL)->all();
+
+    $array_equal = static function ($a, $b) {
+        return serialize($a) === serialize($b);
+    };
+
+    $this->assertFalse($array_equal($movies, $arrays_result));
+
+    $arrays_original = Arrays::create([
+        'movies' => [
+            'the_thin_red_line' => [
+                'title' => 'The Thin Red Line',
+                'directed_by' => 'Terrence Malick',
+                'produced_by' => 'Robert Michael, Geisler Grant Hill, John Roberdeau',
+                'decription' => 'Adaptation of James Jones autobiographical 1962 novel, focusing on the conflict at Guadalcanal during the second World War.',
+            ],
+            'bad_times_at_the_el_royale' => [
+                'title' => 'Bad Times at the El Royale',
+                'directed_by' => 'Drew Goddard',
+                'produced_by' => 'Drew Goddard, Steve Asbell',
+                'decription' => 'Early 1970s. Four strangers check in at the El Royale Hotel. The hotel is deserted, staffed by a single desk clerk. Some of the new guests reasons for being there are less than innocent and some are not who they appear to be.',
+            ],
+        ],
+    ]);
+    $movies          = $arrays_original->get('movies');
+    $arrays_result   = Arrays::create($movies)->sortBySubKey('title', 'DESC', SORT_NATURAL)->all();
+
+    $array_equal = static function ($a, $b) {
+        return serialize($a) === serialize($b);
+    };
+
+    $this->assertTrue($array_equal($movies, $arrays_result));
 });
 
 test('test count() method', function (): void {
